4f365c382ea79613b9da7597ab3b6eb1791934aa
Singlesally <59649898+Singlesally@users.noreply.github.com>
2021-06-08 22:27:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let inputs = document.querySelectorAll('input[type="tel"]');
+let im = new Inputmask('+7 (999) 999-99-99');
+im.mask(inputs);
+
+function formatState (state) {
+    if (!state.id) {
+      return state.text;
+    }
+  
+    var baseUrl = "img/flag";
+    var $state = $(
+      '<span><span></span><img class="img-flag" /> </span>'
+    );
+  
+    $state.find("span").text(state.text);
+    $state.find("img").attr("src", baseUrl + "/" + state.element.value.toLowerCase() + ".png");
+  
+    return $state;
+};
+  
+$(".js-example-templating").select2({templateSelection: formatState});
